ced5ddfa90cd1bfc0eed9f8a44d3933213dd77e3
Clarke <sclarke5555@gmail.com>
2023-11-03 13:42:02 -0400

    remove trello attempt and start over

--- src/app/dashboard/projects/page.tsx
+++ src/app/dashboard/projects/page.tsx
@@ -1,78 +1,14 @@
-'use client'
-
-import React, { useState, useRef } from 'react';
-import { Typography } from '@mui/material';
-import { data, statuses } from './trelloData';
-import { DropWrapper } from './DropWrapper';
-import { Column } from './Column';
-import { TrelloItem } from './TrelloItem';
-import styles from './Projects.module.scss';
+import { Typography } from "@mui/material"
 
 const Projects = () => {
-  const [items, setItems] = useState(data);
-
-  const onDrop = (item: any, monitor: any, status: any) => {
-    if(status === item.status){
-      return;
-    }
-
-    const mapping = statuses.find(si => si.status === status);
-
-    setItems(prevState => {
-      const newItems = prevState
-        .filter(i => i.id !== item.id)
-        .concat({ ...item, status, icon: mapping?.icon });
-      return [ ...newItems ];
-    });
-  };
-
-  const moveItem = (dragIndex: number, hoverIndex: number) => {
-    const item = items[dragIndex];
-    setItems(prevState => {
-      const newItems = prevState.filter((i, idx) => idx !== dragIndex);
-      newItems.splice(hoverIndex, 0, item);
-      return [ ...newItems ];
-    });
-};
-
   return (
-    <>
-      <Typography 
+    <Typography  
         variant='h2' 
         sx={{ 
           marginTop: 10, 
           paddingBottom: 4,
           fontWeight: 600
-        }}
-      >
-        Projects
-      </Typography>
-      <div className={styles.row}>
-        {statuses.map(s => {
-          return (
-            <div key={s.id} className={styles.columnWrapper}>
-              <h2 className={"col-header"}>{s.status.toUpperCase()}</h2>
-                <DropWrapper onDrop={onDrop} status={s.status}>
-                  <Column>
-                    {items
-                      .filter(i => i.status === s.status)
-                      .map((i, idx) => 
-                        <TrelloItem 
-                          key={i.id} 
-                          item={i} 
-                          index={idx} 
-                          moveItem={moveItem} 
-                          status={s} 
-                        />
-                      )
-                    }
-                  </Column>
-                </DropWrapper>
-            </div>
-          );
-        })}
-      </div>
-    </>
+        }}>Profile</Typography>
   )
 }
 
